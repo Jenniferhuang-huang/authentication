d71e6671b74e08957872adf99255ce0a9043d4b2
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-04-04 22:23:04 -0400

    anthentication setup

--- db/users.js
+++ db/users.js
@@ -1,6 +1,19 @@
 var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+  // add users here
+  {
+    id: 1,
+    username: "Westcliff",
+    password: "secret",
+    displayName: "Westcliff",
+    emails: [{ value: "west@example.com" }],
+  },
+  {
+    id: 2,
+    username: "Westcliffclass",
+    password: "birthday",
+    displayName: "Westcliff University",
+    emails: [{ value: "cliff@example.com" }]
+  },
 ];
 
 exports.findById = function(id, cb) {
